0eb66b81c9c67bc4c0b2d32ac6fbcd45eb2ef4ba
Chad Metcalf <chadmetcalf@users.noreply.github.com>
2015-10-26 14:27:27 -0500

    Refactor Scrape model
    None of this stuff is tested. I did a quick refactor to demonstrate a different way to layout this class.
    
    • More descriptive method names (The naming makes it clear that there are some other object living in this model, like a subreddit object and a flickr object; these can be easily abstracted when the time is right)
    • Only initialized in the initializer, moved the scraping down where it is used.
    • Enforced public/private methods. The refactor could possibly break where a Scrape object is used in the Poster controller.
    • Next step might be to move this to a more formal service object. (http://multithreaded.stitchfix.com/blog/2015/06/02/anatomy-of-service-objects-in-rails/).

--- app/models/scrape.rb
+++ app/models/scrape.rb
@@ -1,41 +1,57 @@
 class Scrape
   require 'open-uri'
-  def initialize(sub)
-    if sub == ""
-      @sub = "https://www.reddit.com/"
-    else
-      @sub = "https://www.reddit.com/r/#{sub}"
-    end
-    @textpage = Nokogiri::HTML(open(@sub))
-    @imagepage = Nokogiri::HTML(open('https://www.flickr.com/explore/interesting/7days/'))
-  end
-
-  def link
-    links = @textpage.css('div.link')
-    @link ||= links[rand(25)]
-    @link.css('a.title')
-  end
-
-  def text
-    link.text
-  end
-
-  def url
-    @link.css('a.comments').map { |link| link['href'] }.first
-  end
-
-  def imagelink
-    "https://www.flickr.com#{@imagepage.at_css("td.Owner a")['href']}"
-  end
-
-  def image
-    page = Nokogiri::HTML(open(imagelink))
-    # @image[rand(10)]['href']
-    image = page.at_css("img.main-photo")['src']
-    "http:#{image}"
+  
+  def initialize(subreddit_name)
+    @subreddit_name = subreddit_name
   end
 
   def post
-    Poster.new(text: text, image: image)
+    Poster.new(text: subreddit_text, image: flickr_image)
+  end
+  
+  def reddit_textpage
+    @reddit_textpage ||= Nokogiri::HTML(open(reddit_uri))
+  end
+  
+  def flickr_imagepage
+    @flickr_imagepage ||= Nokogiri::HTML(open('https://www.flickr.com/explore/interesting/7days/'))
+  end
+
+  private
+  
+  def subreddit_link
+    @subreddit_link ||= subreddit_links.sample.css('a.title')
+  end
+
+  def subreddit_links
+    reddit_textpage.css('div.link')
+  end
+  
+  def subreddit_text
+    subreddit_link.text
+  end
+
+  # Unused method?
+  def subreddit_link_url
+    subreddit_link.css('a.comments').map { |link| link['href'] }.first
+  end
+
+  def flickr_imagelink_url
+    "https://www.flickr.com#{flickr_imagepage.at_css("td.Owner a")['href']}"
+  end
+
+  def flickr_image
+    @flickr_image || flickr_image!
+  end
+  
+  def flickr_image!
+    Nokogiri::HTML(open(flickr_imagelink_url)).at_css("img.main-photo")['src']
+    @flickr_image = "http:#{image}"
+  end
+  
+  def reddit_uri
+    return "https://www.reddit.com/" if @subreddit_name.blank?
+
+    "https://www.reddit.com/r/#{@subreddit_name}"
   end
 end
